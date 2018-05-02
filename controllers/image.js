a1df1ece89a2d86390ffa45e8ac4eb42f5671ddb
Essaid <Purelight4ever@gmail.com>
2018-05-02 02:38:52 -0400

    debugging

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+//Clarifai API:
+const app = new Clarifai.App({
+	apiKey: 'a656186cb7c346bda897997e69cf3b72'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => {
+		res.json(data);
+	})
+	.catch(err => res.status(400).json('Unable to work with API'))
+}
+
 const handleImage = (req,res, db) => {
 	const { id } = req.body;
  db('users').where('id', '=', id)
